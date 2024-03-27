d8530493a3bd5ab08dd1998090f4247d03e0d5f9
stephenlit <stevish45@icloud.com>
2024-03-27 09:14:24 -0300

    Added route to get the list of best sellers based on input catagory

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -6,10 +6,14 @@ const getInputChoices = asyncHandler(async (req, res) => {
         `https://api.nytimes.com/svc/books/v3/lists/names.json?q=&api-key=${process.env.API_KEY}`
     );
     const inputChoices = await inputsData.json();
-    res.render('home', {inputChoices});
+    res.render('home', { inputChoices });
 });
 
+const bestSellerList = asyncHandler(async (req, res) => {
+    res.status(200).json({message: 'ok'});
+});
 
 module.exports = {
     getInputChoices,
-}
+    bestSellerList,
+};
--- routes/booksRoutes.js
+++ routes/booksRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 const {
     getInputChoices,
+    bestSellerList,
 } = require('../controllers/dataController');
 
-router
-    .route('/')
-    .get(getInputChoices);
+router.route('/').get(getInputChoices);
+router.route('/:id').post(bestSellerList);
 
 module.exports = router;
