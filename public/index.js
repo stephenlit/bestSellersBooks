99c295ad9de17452178df8b9c1fd8250762bf6a9
stephenlit <stevish45@icloud.com>
2024-03-30 22:08:11 -0300

    added some new code to index.js

--- public/index.js
+++ public/index.js
@@ -1,13 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const boxes = document.querySelectorAll(".box");
-    const delay = 500; 
-    for (var i = 0; i < boxes.length; i++) {
-      (function(index) {
-        setTimeout(function() {
-          boxes[index].style.opacity = 1;
-          boxes[index].style.transform = "translateX(0)";
-        }, delay * index);
-      })(i);
-    }
-  });
+// document.addEventListener("DOMContentLoaded", function() {
+//     const boxes = document.querySelectorAll(".box");
+//     const delay = 500; 
+//     for (const i = 0; i < boxes.length; i++) {
+//       (function(index) {
+//         setTimeout(function() {
+//           boxes[index].style.opacity = 1;
+//           boxes[index].style.transform = "translateX(0)";
+//         }, delay * index);
+//       })(i);
+//     }
+//   });
   
+
+ function modifyAnimation() {
+    const boxes = document.querySelectorAll(".box");
+    for (let i = 0; i < boxes.length; i++) {
+      const box = boxes[i];
+      box.style.setProperty("animation", `1s forwards ${i * 500}ms slideIn`);
+  }
+}
+
+window.addEventListener("load", () => {
+  modifyAnimation();                    
+})
